4de353827599a59442c62002e1be92c90b99600c
Daniil Kokorev <dakokorev@rosatom.ru>
2023-01-26 14:39:21 +0300

    Commit 2

--- src/main/java/lvl2les2/Main.java
+++ src/main/java/lvl2les2/Main.java
@@ -43,6 +43,7 @@ public class Main {
     public static void getArraySum(String[][] array) throws MyArrayDataException, MyArraySizeException {
 
 
+        //Commit 2
         //AADAD
 
         try {
